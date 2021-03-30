53b68879020f9661e3724f12ce6135967086c547
dtonghm <dtong@happymoney.com>
2021-03-30 11:57:11 -0700

    removed some code

--- istanbul-mocha/test/test.js
+++ istanbul-mocha/test/test.js
@@ -8,7 +8,4 @@ describe("simple", function(){
   it("should return false", function(){
     expect(simple.math(0)).to.equal(false);
   });
-  it("should return true", function(){
-    expect(simple.math(1)).to.equal(false);
-  });
 });
